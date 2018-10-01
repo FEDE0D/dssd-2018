6d8b87098d9e089c222d8560c4d55ac2c021c206
Federico <federico@ensolvers.com>
2018-10-01 13:52:40 -0300

    update recreate schema script to work with docker

--- recreate-schema.sh
+++ recreate-schema.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "[INFO] Creating DB..."
-mysql -uroot -e "drop database if exists dssd; create database dssd;"
+mysql -uroot -h127.0.0.1 -e "drop database if exists dssd; create database dssd;"
 
 echo "[INFO] Importing schema..."
-mysql -uroot dssd < schema/dump-coupons-201808030751.sql
-mysql -uroot dssd < schema/dump-rrhh-201808030750.sql
-mysql -uroot dssd < schema/dump-stock-201808030748.sql
+mysql -uroot -h127.0.0.1 dssd < schema/dump-coupons-201808030751.sql
+mysql -uroot -h127.0.0.1 dssd < schema/dump-rrhh-201808030750.sql
+mysql -uroot -h127.0.0.1 dssd < schema/dump-stock-201808030748.sql
 
 echo "[DONE]"
 
